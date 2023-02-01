f503a7a911b850882ba4aed1c393afc84870c854
Zolotarev <zolotarevmixail@rambler.ru>
2023-02-01 18:47:13 +0300

    created elements_test / TextBox added elements_page.py, elements_page_locators.py, elements_test.py, generator.py, data.py

---
+++ tests/elements_test.py
@@ -0,0 +1,20 @@
+import time
+
+from pages.elements_page import TextBoxPage
+
+
+class TestElements:
+    class TestTrxtBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_current_address, "the current_address does not match"
+            assert permanent_address == output_permanent_address, "the permanent_address does not match"
+
+            time.sleep(5)
+
